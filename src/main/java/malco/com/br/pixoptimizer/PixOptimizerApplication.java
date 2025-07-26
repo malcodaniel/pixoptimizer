10384f3b63d33206f03d7dba14c5677fcd2c46bc
Malco <malcodantas18@gmail.com>
2025-07-26 01:10:36 -0400

    Create database connection and environment setup

--- src/main/java/malco/com/br/pixoptimizer/PixOptimizerApplication.java
+++ src/main/java/malco/com/br/pixoptimizer/PixOptimizerApplication.java
@@ -3,11 +3,31 @@ package malco.com.br.pixoptimizer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class PixOptimizerApplication {
+    public static final String GCP_PROJECT_DEVELOPMENT = "pixoptimizer";
 
     public static void main(String[] args) {
-        SpringApplication.run(PixOptimizerApplication.class, args);
+        Logger log = Logger.getLogger(PixOptimizerApplication.class.getSimpleName());
+        String gcpProject = System.getenv(GCP_PROJECT_DEVELOPMENT);
+
+        if (gcpProject == null) {
+            gcpProject = "N/A";
+        }
+
+        String activeProfile;
+        if (GCP_PROJECT_DEVELOPMENT.equals(gcpProject)) {
+            activeProfile = "development";
+        } else {
+            activeProfile = "local";
+        }
+
+        log.info("GCP project = " + gcpProject + ", Active Profile = " + activeProfile);
+        SpringApplication app = new SpringApplication(PixOptimizerApplication.class);
+        app.setAdditionalProfiles(activeProfile);
+        app.run(args);
     }
 
 }
--- src/main/java/malco/com/br/pixoptimizer/controllers/TestController.java
+++ src/main/java/malco/com/br/pixoptimizer/controllers/TestController.java
@@ -1,2 +1,12 @@
-package malco.com.br.pixoptimizer.controllers;public class TestController {
+package malco.com.br.pixoptimizer.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping
+    public String test() {
+        return "teste";
+    }
 }
